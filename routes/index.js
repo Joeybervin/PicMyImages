7db95405134a8614a7f6cd2d806ea67c9793b7f2
Joey Bervin <61996287+Joeybervin@users.noreply.github.com>
2022-08-05 09:56:27 +0200

    components creation
    création des différents composants de l'app

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+/* API KEYS */
+const unsplashApiKey = "E1z8KRKXgbH6GBVDNF-InjgQqYmGpSPzHUmho78Wf8o";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
